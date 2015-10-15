82fe3439ad20152f1a62ebabeee071ed9a28e27c
adamraet <adamraet@buffalo.edu>
2015-10-15 16:02:28 -0400

    fuck off Linus

---
+++ Unity_Project/Assets/_Scripts/LanceController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LanceController : MonoBehaviour {
+
+    public float speed = 5f;
+
+
+	
+	
+	void FixedUpdate () {
+
+        float moveH = Input.GetAxis("Horizontal");
+
+        float moveV = Input.GetAxis("Vertical");
+
+        moveH = Mathf.Clamp(moveH, -10, 10);
+        moveV = Mathf.Clamp(moveV, -45, 45);
+
+
+        transform.Rotate(moveV * speed * Time.deltaTime, moveH * speed * Time.deltaTime, 0);
+
+	}
+}
---
+++ Unity_Project/Assets/_Scripts/LancePivot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LancePivot : MonoBehaviour {
+
+    /* 
+        Creates a customized pivot point. 
+        Used to simulate the rotation and aiming
+        of the player's lance from where the 
+        lance would be held.
+    */
+
+        public float size = 1.0f;
+        public Color color = Color.yellow;
+
+        void OnDrawGizmos ()
+        {
+            Gizmos.color = color;
+            Gizmos.DrawWireSphere(transform.position, size);
+
+        }
+
+	
+}
