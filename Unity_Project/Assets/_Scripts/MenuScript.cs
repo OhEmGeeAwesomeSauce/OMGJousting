2b35f52f3dbf52d0d97bf79d7de607d28cc98ce3
Charles King <csking3@buffalo.edu>
2015-11-10 14:58:20 -0500

    Implement start menu

---
+++ Unity_Project/Assets/_Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript: MonoBehaviour
+{
+
+    public Canvas quitMenu; //not used yet
+    public Button startText;
+    public Button exitText;
+
+    // Use this for initialization
+    void Start()
+    {
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+    }
+
+    public void StartGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
