92ac50b78ed1aae713a06dfc3273c162b8af1ad4
korbengr <korbengrover@gmail.com>
2015-12-10 16:21:35 -0500

    Added endmenu
    Hopefully this doesn't break anything.

---
+++ Unity_Project/Assets/_MainScene/_Scripts/PlayerNetworkMover.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNetworkMover : Photon.MonoBehaviour {
+
+
+    Vector3 position;
+    Quaternion rotation;
+    float smoothing = 10f;
+    float health = 100f;
+
+
+    void Start()
+    {
+
+        if (photonView.isMine)
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<NavAgent>().enabled = true;
+            GetComponent<AccelerateTrigger2>().enabled = true;
+            GetComponentInChildren<LanceFollow>().enabled = true;
+
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponentInChildren<HeadLook>().enabled = true;
+            GetComponentInChildren<LanceController>().enabled = true;
+            GetComponentInChildren<LanceFollow>().enabled = true;
+
+            foreach (Camera cam in GetComponentsInChildren<Camera>())
+                cam.enabled = true;
+        }
+        else
+        {
+            StartCoroutine("UpdateData");
+        }
+    }
+
+    IEnumerator UpdateData()
+    {
+        while (true)
+        {
+            transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * smoothing);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * smoothing);
+            yield return null;
+        }
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(health);
+        }
+        else
+        {
+            position = (Vector3)stream.ReceiveNext();
+            rotation = (Quaternion)stream.ReceiveNext();
+            health = (float)stream.ReceiveNext();
+        }
+    }
+
+
+}
